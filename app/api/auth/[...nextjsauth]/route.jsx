5ff32b5aaf7d8b4aa98335b856d4fd923d7e9f07
Pankaj Mittal <114716727+pmittal77@users.noreply.github.com>
2024-08-15 22:07:04 +0530

    Update route.jsx

---
+++ app/api/auth/[...nextjsauth]/route.jsx
@@ -0,0 +1,11 @@
+import NextAuth from "next-auth/next";
+import GoogleProviders from 'next-auth/providers/google';
+
+const handler = NextAuth({
+  providers: [
+    GoogleProviders({
+      clientId: '',
+      clientSecret: '',
+    })
+  ]
+})
